452f59d3ec2048ba439b788e8e3207a7aed40508
MD. Mehedi Hasan Shuvo <shuvo@asl.aero>
2023-06-20 15:24:22 +0600

    Update Test BaseCase class

--- tests/BaseCase.py
+++ tests/BaseCase.py
@@ -3,6 +3,9 @@ from app import app
 
 class BaseCase(unittest.TestCase):
     def setUp(self):
+        app.config.update({
+            "TESTING": True
+        })
         self.app = app.test_client()
 
     def tearDown(self) -> None:
